2fe2c46ff137dd145f0307ed1913b49229fea638
miathedev <mia@strongforce.de>
2021-03-05 10:57:25 +0100

    Minor fies

--- filament.go
+++ filament.go
@@ -15,7 +15,7 @@ type Filament struct {
 	Price    uint   `gorm:"check:price >= 0"`
 }
 
-func CreateFilamentRGBRaw(name string, material string, r uint8, g uint8, b uint8, weight uint, slot uint) Filament {
+func CreateFilamentRGBRaw(name string, material string, r uint8, g uint8, b uint8, weight uint, slot uint, price uint) Filament {
 	var colorRGB = colors.RGBColor{R: r, G: g, B: b}
 	var spoolColor string = colorRGB.ToHEX().String()
 
@@ -24,14 +24,14 @@ func CreateFilamentRGBRaw(name string, material string, r uint8, g uint8, b uint
 	return Filament
 }
 
-func CreateFilamentRGB(name string, material string, color colors.RGBColor, weight uint, slot uint) Filament {
+func CreateFilamentRGB(name string, material string, color colors.RGBColor, weight uint, slot uint, price uint) Filament {
 	var spoolColor string = color.ToHEX().String()
 	Filament := Filament{Name: name, Material: material, Color: spoolColor, Weight: weight, Slot: slot}
 
 	return Filament
 }
 
-func CreateFilamentRGBHex(name string, material string, color string, weight uint, slot uint) Filament {
+func CreateFilamentRGBHex(name string, material string, color string, weight uint, slot uint, price uint) Filament {
 	spoolColor, err := colors.ParseHEX(color)
 	if err != nil {
 		spoolColor, _ = colors.ParseHEX("#000000") //Black
